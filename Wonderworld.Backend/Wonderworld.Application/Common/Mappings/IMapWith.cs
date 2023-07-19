808f6838c49074b16c18585eda71fffd3664760f
Ales Ban <alesbah04@gmail.com>
2023-07-19 22:18:05 +0300

    start mediator pattern + CQRS

---
+++ Wonderworld.Backend/Wonderworld.Application/Common/Mappings/IMapWith.cs
@@ -0,0 +1,9 @@
+using AutoMapper;
+
+namespace Wonderworld.Application.Common.Mappings;
+
+public interface IMapWith<T>
+{
+    void Mapping(Profile profile) => 
+        profile.CreateMap(typeof(T), GetType());
+}
