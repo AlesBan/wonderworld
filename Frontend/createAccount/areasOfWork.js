a1733e8444b84c0406c3f4efebc03598c0921b79
Klimpavlov <Klimpavlov2002@gmail.com>
2023-08-25 18:30:03 +0300

    started lessons-list

--- Frontend/createAccount/areasOfWork.js
+++ Frontend/createAccount/areasOfWork.js
@@ -13,19 +13,49 @@ function chooseWork() {
     <div class="inputs">
       <div class="first-name">
         <div class="input-title">Areas of work</div>
-        <div class="select">
-<!--          <label><input name="first-name" class="first-name-input" type="text" placeholder="Select.."></label>-->
-<!--          <div class=""><img src="../images/chevron-down.svg" alt=""></div>-->
-          <select>
-           <option value="" disabled selected>Select..</option>
-           <option>Пункт 1</option>
-           <option>Пункт 2</option>
-           </select>
+<!--        <div class="select">-->
+<!--&lt;!&ndash;          <label><input name="first-name" class="first-name-input" type="text" placeholder="Select.."></label>&ndash;&gt;-->
+<!--&lt;!&ndash;          <div class=""><img src="../images/chevron-down.svg" alt=""></div>&ndash;&gt;-->
+<!--          <select>-->
+<!--           <option value="" disabled selected>Select..</option>-->
+<!--           <option>Пункт 1</option>-->
+<!--           <option>Пункт 2</option>-->
+<!--           </select>-->
+<div class="select-btn">
+    <span class="btn-text">Select..</span>
+    <span class="arrow-dwn">
+        <img src="../images/chevron-down.svg" alt="">
+    </span>
+</div>
+<ul class="list-items">
+    <li class="item">
+        <span class="item-text">Art</span>
+        <span class="checkbox"></span>
+    </li>
+     <li class="item">
+        <span class="item-text">Biology</span>
+        <span class="checkbox"></span>
+    </li>
+     <li class="item">
+        <span class="item-text">Chemistry</span>
+        <span class="checkbox"></span>
+    </li>
+     <li class="item">
+        <span class="item-text">Computer science</span>
+        <span class="checkbox"></span>
+    </li>
+</ul>
         </div>
       </div>
     </div>
     <button onclick="" class="primary-button">Continue</button>
   </div>
     `
+    const selectBtn = document.querySelector('.select-btn');
+    items = document.querySelectorAll('.item');
+
+    selectBtn.addEventListener("click", () => {
+        selectBtn.classList.toggle("open");
+    })
 }
 
