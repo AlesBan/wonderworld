facdbc38ef5aee31c29d81862665da21b555c634
Klimpavlov <Klimpavlov2002@gmail.com>
2023-09-21 19:34:42 +0300

    started profile-settings

--- Frontend/createAccount/dataGrades.js
+++ Frontend/createAccount/dataGrades.js
@@ -1,67 +1,67 @@
 const ALL_GRADES = [
     {
         id: 1,
-        classNum: 0,
         age: '5-6',
+        classNum: 0,
     },
     {
         id: 2,
-        classNum: 1,
         age: '6-7',
+        classNum: 1,
     },
     {
         id: 3,
-        classNum: 2,
         age: '7-8',
+        classNum: 2,
     },
     {
         id: 4,
-        classNum: 3,
         age: '8-9',
+        classNum: 3,
     },
     {
         id: 5,
-        classNum: 4,
         age: '9-10',
+        classNum: 4,
     },
     {
         id: 6,
-        classNum: 5,
         age: '10-11',
+        classNum: 5,
     },
     {
         id: 7,
-        classNum: 6,
         age: '11-12',
+        classNum: 6,
     },
     {
         id: 8,
-        classNum: 7,
         age: '12-13',
+        classNum: 7,
     },
     {
         id: 9,
-        classNum: 8,
         age: '13-14',
+        classNum: 8,
     },
     {
         id: 10,
-        classNum: 9,
         age: '14-15',
+        classNum: 9,
     },
     {
         id: 11,
-        classNum: 10,
         age: '15-16',
+        classNum: 10,
     },
     {
         id: 12,
-        classNum: 11,
         age: '16-17',
+        classNum: 11,
     },
     {
         id: 13,
-        classNum: 12,
         age: '17-18',
+        classNum: 12,
     },
 ]
