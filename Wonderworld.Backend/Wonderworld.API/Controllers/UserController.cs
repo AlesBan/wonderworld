60d1cb13643859429727316fba85f0d19af1cc36
Ales Ban <alesbah04@gmail.com>
2023-09-30 15:27:57 +0300

    delete user + UserController update

--- Wonderworld.Backend/Wonderworld.API/Controllers/UserController.cs
+++ Wonderworld.Backend/Wonderworld.API/Controllers/UserController.cs
@@ -3,13 +3,13 @@ using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 using Wonderworld.API.Helpers.JwtHelpers;
 using Wonderworld.API.Services.AccountServices;
+using Wonderworld.Application.Dtos.AuthenticationDtos;
 using Wonderworld.Application.Dtos.CreateAccountDtos;
 using Wonderworld.Application.Interfaces;
 using Wonderworld.Domain.Entities.Main;
 
 namespace Wonderworld.API.Controllers;
 
-[Authorize]
 public class UserController : BaseController
 {
     private readonly ISharedLessonDbContext _sharedLessonDbContext;
@@ -27,11 +27,34 @@ public class UserController : BaseController
         return await _sharedLessonDbContext.Users.ToListAsync();
     }
 
+    [HttpPost("login")]
+    public async Task<IActionResult> Login([FromBody] UserLoginRequestDto requestUserDto)
+    {
+        return await _userAccountService.LoginUser(requestUserDto, Mediator);
+    }
+
+    [HttpPost("register")]
+    public async Task<IActionResult> Register([FromBody] UserRegisterRequestDto requestUserDto)
+    {
+        return await _userAccountService.RegisterUser(requestUserDto, Mediator);
+    }
+
+    [Authorize]
     [HttpPost("create-account")]
     public async Task<IActionResult> CreateAccount([FromBody] UserCreateAccountRequestDto requestUserDto)
     {
         var userId = JwtHelper.GetUserIdFromClaims(HttpContext);
         var result = await _userAccountService.CreateUserAccount(userId, requestUserDto, Mediator);
+
+        return Ok(result);
+    }
+
+    [Authorize]
+    [HttpDelete("delete-user")]
+    public async Task<IActionResult> DeleteUser()
+    {
+        var userId = JwtHelper.GetUserIdFromClaims(HttpContext);
+        var result = await _userAccountService.DeleteUser(userId, Mediator);
 
         return Ok(result);
     }
--- Wonderworld.Backend/Wonderworld.API/Services/AccountServices/IUserAccountService.cs
+++ Wonderworld.Backend/Wonderworld.API/Services/AccountServices/IUserAccountService.cs
@@ -2,7 +2,6 @@ using MediatR;
 using Microsoft.AspNetCore.Mvc;
 using Wonderworld.Application.Dtos.AuthenticationDtos;
 using Wonderworld.Application.Dtos.CreateAccountDtos;
-using Wonderworld.Domain.Entities.Main;
 
 namespace Wonderworld.API.Services.AccountServices;
 
