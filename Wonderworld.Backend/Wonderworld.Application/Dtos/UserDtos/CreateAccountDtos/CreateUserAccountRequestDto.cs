183827038ac307686725ca6f4fc6ee1a69a8922a
Ales Ban <alesbah04@gmail.com>
2023-11-01 12:58:29 +0300

    AtLeastOfPositionShouldBeTrue

--- Wonderworld.Backend/Wonderworld.Application/Dtos/UserDtos/CreateAccountDtos/CreateUserAccountRequestDto.cs
+++ Wonderworld.Backend/Wonderworld.Application/Dtos/UserDtos/CreateAccountDtos/CreateUserAccountRequestDto.cs
@@ -1,4 +1,5 @@
 using System.ComponentModel.DataAnnotations;
+using Wonderworld.Application.Attributes;
 using Wonderworld.Application.Dtos.CreateAccountDtos;
 using Wonderworld.Application.Dtos.InstitutionDtos;
 
@@ -6,15 +7,21 @@ namespace Wonderworld.Application.Dtos.UserDtos.CreateAccountDtos;
 
 public class CreateUserAccountRequestDto
 {
-    [Required] public string FirstName { get; set; }
-    [Required] public string LastName { get; set; }
-    [Required] public bool IsATeacher { get; set; }
-    [Required] public bool IsAnExpert { get; set; }
-    [Required] public string CityLocation { get; set; }
-    [Required] public string CountryLocation { get; set; }
+    [Required] public string FirstName { get; set; } = string.Empty;
+    [Required] public string LastName { get; set; } = string.Empty;
+
+    [Required]
+    [AtLeastOneOfPositionTrue("IsATeacher", "IsAnExpert")]
+    public bool IsATeacher { get; set; }
+
+    [Required]
+    [AtLeastOneOfPositionTrue("IsATeacher", "IsAnExpert")]
+    public bool IsAnExpert { get; set; }
+    [Required] public string CityLocation { get; set; } = string.Empty;
+    [Required] public string CountryLocation { get; set; } = string.Empty;
     [Required] public InstitutionDto InstitutionDto { get; set; }
     [Required] public IEnumerable<string> Disciplines { get; set; } = new List<string>();
     [Required] public IEnumerable<string> Languages { get; set; } = new List<string>();
     [Required] public IEnumerable<int> Grades { get; set; } = new List<int>();
-    [Required] public string PhotoUrl { get; set; }
+    [Required] public string PhotoUrl { get; set; } = string.Empty;
 }
